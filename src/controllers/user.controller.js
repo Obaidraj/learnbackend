e058da46179a31f5de1296b589c194c1c420613f
iamobaidtariq <iamobaidtariq@gmail.com>
2024-10-03 13:08:59 +0500

    Api implementation of user registration without testing and resolve miner bugs

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -51,7 +51,9 @@ const registerUser = asyncHandler(async (req, res) => {
   if (!createdUser) {
     throw new ApiError(500, "Something went wrong while registering the user");
   }
-
+return res.status(201).json(
+    new ApiResponse(200,createdUser,'User registered Successfully')
+)
 });
 
 export { registerUser };
